b6b3f608cf1324608f443f055347fb46ced307f7
Dicky Herlambang <herlambangdicky5@gmail.com>
2017-09-23 06:32:56 +0700

    Revert back change HIDL

--- product/qcom-display.mk
+++ product/qcom-display.mk
@@ -1,5 +1,9 @@
 # Display QCOM packages
 PRODUCT_PACKAGES += \
+     android.hardware.graphics.allocator@2.0-impl \
+     android.hardware.graphics.composer@2.1-impl \
+     android.hardware.graphics.mapper@2.0-impl \
+     android.hardware.memtrack@1.0-impl \
     hwcomposer.msm8960 \
     gralloc.msm8960 \
     copybit.msm8960 \
