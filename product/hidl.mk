0617c99d182161f5cb380b1e59397e4e148a0e2c
Dicky Herlambang <herlambangdicky5@gmail.com>
2018-03-02 03:53:46 -0500

    nicki: HIDL rebuilt
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- product/hidl.mk
+++ product/hidl.mk
@@ -1,3 +1,7 @@
+# HIDL
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
+
 # Configstore
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-impl
@@ -14,6 +18,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl
 
-# HIDL
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
+# Display
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl
