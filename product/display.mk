9b7a0a84adf1d3b49dd6c8eea8389536382964d9
Dicky Herlambang <herlambangdicky5@gmail.com>
2018-03-02 19:52:32 -0500

    sepolicy: disable for nicki
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- product/display.mk
+++ product/display.mk
@@ -2,7 +2,7 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.composition.type=dyn \
     persist.debug.wfd.enable=1 \
+    debug.hwui.use_buffer_age=false \
     persist.hwc.mdpcomp.enable=true \
     persist.sys.wfd.virtual=0 \
-    ro.sf.lcd_density=240 \
-    debug.hwui.use_buffer_age=false
+    ro.sf.lcd_density=240 
