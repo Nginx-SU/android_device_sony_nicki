f481343b8818e16efb56accf05c09f4a3928a6e4
Dicky Herlambang <herlambangdicky5@gmail.com>
2018-03-06 18:15:00 +0700

    nicki: Move lights rule to own unit
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- product/hardware.mk
+++ product/hardware.mk
@@ -6,12 +6,3 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.qcom.baseband.sh:system/etc/init.qcom.baseband.sh \
     $(LOCAL_PATH)/rootdir/init.class_main.sh:root/init.class_main.sh
-
-# Lights package
-PRODUCT_PACKAGES += \
-    lights.msm8960
-
-# Lights HIDL interfaces
-PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service
