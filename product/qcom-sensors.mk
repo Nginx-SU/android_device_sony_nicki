c7f2c987a654d9037ceb770b1a0d195894602a29
Dicky Herlambang <herlambangdicky5@gmail.com>
2018-03-06 18:29:18 +0700

    vendor: Move device specific system configs into /vendor/etc
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- product/qcom-sensors.mk
+++ product/qcom-sensors.mk
@@ -1,7 +1,7 @@
 # Sensors permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.proximity.xml
 
 # Sensors HIDL interfaces
 PRODUCT_PACKAGES += \
