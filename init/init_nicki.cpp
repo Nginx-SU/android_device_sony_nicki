71d1dddc61302a1f67fe60fc3ac21dc8175699cb
Dicky Herlambang <herlambangdicky5@gmail.com>
2018-03-09 17:27:28 +0700

    Revert "nicki: Allow vendor init to override props"
    This reverts commit 562b5337cb73895246ab9045dd1d0048a7f558a9.

--- init/init_nicki.cpp
+++ init/init_nicki.cpp
@@ -31,8 +31,6 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
-#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
-#include <sys/_system_properties.h>
 
 #include "vendor_init.h"
 #include "property_service.h"
@@ -74,17 +72,6 @@ void ds_properties()
 {
         property_set("persist.radio.multisim.config", "dsds");
         property_set("persist.radio.dont_use_dsd", "true");
-}
-
-void property_override(char const prop[], char const value[])
-{
-    prop_info *pi;
-
-    pi = (prop_info*) __system_property_find(prop);
-    if (pi)
-        __system_property_update(pi, value, strlen(value));
-    else
-        __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
 void vendor_load_properties()
